4d632add822461d70dd1612fe13b96d1097871c8
leedavi <dcl@nevoweb.com>
2018-06-11 17:15:18 +0200

    move to UTF8

--- Components/ProviderUtils.cs
+++ Components/ProviderUtils.cs
@@ -104,7 +104,7 @@ namespace NBrightSystemPay
         public static string GetSignature(string strMacCalc)
         {
             System.Security.Cryptography.SHA1CryptoServiceProvider objCrypt = new System.Security.Cryptography.SHA1CryptoServiceProvider();
-            byte[] bytesToHash = System.Text.Encoding.ASCII.GetBytes(strMacCalc);
+            byte[] bytesToHash = System.Text.Encoding.UTF8.GetBytes(strMacCalc);
             string rtnStr = "";
             bytesToHash = objCrypt.ComputeHash(bytesToHash);
             foreach (byte b in bytesToHash)
